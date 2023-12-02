dfa713e28ab9c01d2dfe236a51abc0775737953b
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2023-12-02 16:49:20 -0500

    try to fix codespaces setup

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Add brew to $PATH
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/codespace/.profile
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/codespace/.bashrc
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
