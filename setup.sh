afef643d0605693d04956c3599b324cfa8ffc0d0
Martin Laptev <129316885+maptv@users.noreply.github.com>
2026-06-11 12:13:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -49,5 +49,3 @@ yes n | /bin/bash -c "$(curl -s https://raw.githubusercontent.com/lunarvim/lunar
 
 ## Install Ewka font
 git clone --depth 1 https://github.com/maptv/ewka ~/maptv/ewka && cd ~/whjvenyl/fasd && mkdir ~/.fonts && cp ~/maptv/ewka/nerd/* ~/.fonts && fc-cache -f -v
-
-pip install neovim amazon-codewhisperer-jupyterlab-ext
